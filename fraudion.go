fd2318e4a0b7b3e7a6c93758ed06cc7b9da22e76
André Maricato <am@voipit.pt>
2016-03-21 07:26:09 +0000

    Started work on simple configuration sanity check + now the main thread sleeps after setting up everything, so that the CPU stays away from 100%

--- fraudion.go
+++ fraudion.go
@@ -24,7 +24,8 @@ const (
 
 // Defines expected CLI arguments/flags
 var (
-	configDir = flag.String("config_dir", CONFIG_DIR, "<help message for 'config_dir'>")
+	supported_softwares = []string{"*ast_el_2.3", "*ast_alone_1.8"}
+	configDir           = flag.String("config_dir", CONFIG_DIR, "<help message for 'config_dir'>")
 )
 
 // Config Data Holders
@@ -36,7 +37,45 @@ type configJsonGeneral struct {
 	Default_action_chain_run_count    string
 }
 
+type ConfigGeneral struct {
+	Monitored_software                string
+	Cdrs_source                       string
+	Default_trigger_check_period      time.Duration
+	Default_action_chain_sleep_period time.Duration
+	Default_action_chain_run_count    uint32
+}
+
 type configJsonTriggers struct {
+	Simultaneous_calls     map[string]interface{}
+	Dangerous_destinations map[string]interface{}
+	Expected_destinations  map[string]interface{}
+	Small_duration_calls   map[string]interface{}
+}
+
+type triggerSimultaneousCalls struct {
+	Check_period       time.Duration
+	Max_call_threshold uint32
+}
+
+type triggerDangerousDestinations struct {
+	Check_period  time.Duration
+	Prefix_list   []string
+	Hit_threshold uint32
+}
+
+type triggerExpectedDestinations struct {
+	Check_period  time.Duration
+	Prefix_list   []string
+	Hit_threshold uint32
+}
+
+type triggerSmallCallDurations struct {
+	Check_period       time.Duration
+	Duration_threshold time.Duration
+	Hit_threshold      uint32
+}
+
+type configTriggers struct {
 	Simultaneous_calls     map[string]interface{}
 	Dangerous_destinations map[string]interface{}
 	Expected_destinations  map[string]interface{}
@@ -57,14 +96,35 @@ type configJsonActionHttp struct {
 	Default_parameters map[string]string
 }
 
-type configActionLocalCommands struct {
-	list map[string]string
-}
-
 type configJsonActions struct {
 	Email          configJsonActionEmail
 	Call           configJsonActionCall
 	Http           configJsonActionHttp
+	Local_commands map[string]string
+}
+
+type configActionEmail struct {
+	Default_message string
+}
+
+type configActionCall struct {
+	Default_message string
+}
+
+type configActionHttp struct {
+	Default_url        string
+	Default_method     string
+	Default_parameters map[string]string
+}
+
+type configActionLocalCommands struct {
+	List map[string]string
+}
+
+type configActions struct {
+	Email          configActionEmail
+	Call           configActionCall
+	Http           configActionHttp
 	Local_commands map[string]string
 }
 
@@ -74,9 +134,33 @@ type configActionChainAction struct {
 	Command  string
 }
 
-type configJsonActionChains map[string][]configActionChainAction
+type configActionChain struct {
+	Chains map[string][]configActionChainAction
+}
+
+type configJsonActionChainAction struct {
+	Action   string
+	Contacts []string
+	Command  string
+}
+
+type configJsonActionChains map[string][]configJsonActionChainAction
 
 type configJsonContacts map[string]map[string]interface{}
+
+type configContact struct {
+	For_actions     []string
+	Phone_number    string
+	Email           string
+	Message         string
+	Http_url        string
+	Http_method     string
+	Http_parameters map[string]string
+}
+
+type configContacts struct {
+	List map[string]configContact
+}
 
 // Starts here!
 func main() {
@@ -119,6 +203,7 @@ func main() {
 		fmt.Printf("There was an error opening the config file (\"fraudion.json\") at %s. :(\n", *configDir)
 		os.Exit(-1)
 	}
+	defer configFile.Close()
 
 	var jsonConfig map[string]*json.RawMessage // Better than using the Lib example's Empty interface... https://tour.golang.org/methods/14
 	jsonConfigReader := JsonConfigReader.New(configFile)
@@ -219,6 +304,26 @@ func main() {
 	fmt.Println()
 
 	// TODO Parse configs for acceptable values.
+	// General
+	/*
+	   "monitored_software": "*ast_el_2.3.0", // ast_alone_<version>: Asterisk from <version> source, ast_el_<version>: asterisk from Elastix <version>, fs_<version>: Freeswitch <version>
+	   "cdrs_source": "*db_mysql", // db_mysql: Database MySQL. TODO (v2) json: Json, csv: CSVs
+	   "default_trigger_check_period": "10s", // Can be overriden in Trigger configuration
+	   "default_action_chain_sleep_period": "30m", // Waits this time before checking if Chain run account is still > 0 the Action Chain for a match of some Trigger
+	   "default_action_chain_run_count": "3" // Count for the Reprocessing of the Action Chains
+	*/
+	configGeneral := new(ConfigGeneral)
+
+	fmt.Println("sadjaçidjfsdf", cfgGeneral.Monitored_software)
+	fmt.Println("sadjaçidjfsdf", supported_softwares)
+	found := stringInSlice(cfgGeneral.Monitored_software, supported_softwares)
+	if found == false {
+		// TODO Log this to Syslog
+		fmt.Println("Could not find configured \"monitored_software\" in supported software list. It could be the syntax or you're system may not be supported. :(")
+		os.Exit(-1)
+	}
+	configGeneral.Monitored_software = cfgGeneral.Monitored_software
+
 	simultaneous_calls_check_period, found := cfgTriggers.Simultaneous_calls["check_period"].(string)
 	if found == false {
 		// TODO Confirm that cfgGeneral.Default_trigger_check_period has some value, because this var needs some.
@@ -241,6 +346,7 @@ func main() {
 		fmt.Println("Could not calculate duration for \"simultaneous_calls_check_period_ticker\". :(")
 		os.Exit(-1)
 	}
+
 	simultaneous_calls_check_period_ticker := time.NewTicker(simultaneous_calls_check_period_ticker_duration)
 	go func() { // TODO Future simultaneous_calls_checker()
 		for t := range simultaneous_calls_check_period_ticker.C {
@@ -254,6 +360,7 @@ func main() {
 		fmt.Println("Could not calculate duration for \"dangerous_destinations_check_period_ticker\". :(")
 		os.Exit(-1)
 	}
+
 	dangerous_destinations_check_period_ticker := time.NewTicker(dangerous_destinations_check_period_ticker_duration)
 	go func() { // TODO Future dangerous_destinations_checker()
 		for t := range dangerous_destinations_check_period_ticker.C {
@@ -262,8 +369,19 @@ func main() {
 	}()
 
 	for {
+		// Main "thread" has to Sleep or else 100% CPU, as expected!
+		time.Sleep(100000 * time.Hour)
 	}
 
 	//os.Exit(1)
 
+}
+
+func stringInSlice(str string, list []string) bool {
+	for _, v := range list {
+		if v == str {
+			return true
+		}
+	}
+	return false
 }
